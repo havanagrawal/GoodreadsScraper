646d179af6356a3cc7c78789c0038f057ba29300
Havan <havanagrawal@gmail.com>
2017-11-09 10:59:48 -0800

    Add page number argument to list spider

--- GoodreadsScraper/spiders/list_spider.py
+++ GoodreadsScraper/spiders/list_spider.py
@@ -6,15 +6,14 @@ GOODREADS_URL_PREFIX = "https://www.goodreads.com"
 
 class ListSpider(scrapy.Spider):
     name = "list"
-    start_urls = ["https://www.goodreads.com/list/show/264.Books_That_Everyone_Should_Read_At_Least_Once"]
 
-    def __init__(self):
+    def __init__(self, page_no):
         self.book_spider = BookSpider()
+        list_name = "264.Books_That_Everyone_Should_Read_At_Least_Once"
+        self.start_urls = ["https://www.goodreads.com/list/show/{}?page={}".format(list_name, page_no)]
 
     def parse(self, response):
         list_of_books = response.css("a.bookTitle::attr(href)").extract()
-        print(list_of_books)
-        #yield {"link": GOODREADS_URL_PREFIX + x}
 
         for book in list_of_books:
-            yield response.follow(book, callback=book_spider.parse)
+            yield response.follow(book, callback=self.book_spider.parse)
