e4a64bd51ecf72d4618e45e4d1733dbf28a0a4d4
Havan <havanagrawal@gmail.com>
2017-11-11 11:46:31 -0800

    Add convenience shell script for scraper

---
+++ run_scraper.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Runs the scraper for the given pages
+
+if [[ $# -lt 3 ]]; then
+	echo "Usage: $0 <list_name> <start_page> <end_page> <json_file_prefix>";
+	exit 1
+fi
+
+LIST_NAME=$1
+START_PAGE=$2
+END_PAGE=$3
+JSON_FILE_PREFIX=$4
+
+if [[ ${#START_PAGE} -lt 2 ]] ; then
+			START_PAGE="0${START_PAGE}"
+			START_PAGE="${START_PAGE: -2}"
+fi
+
+if [[ ${#END_PAGE} -lt 2 ]] ; then
+			END_PAGE="0${END_PAGE}"
+			END_PAGE="${END_PAGE: -2}"
+fi
+
+scrapy crawl list -s DOWNLOAD_DELAY=0 -s LOG_FILE=scrapy_2017_11_09.log -o "$JSON_FILE_PREFIX"_"$START_PAGE"_"$END_PAGE".json -a start_page_no=$START_PAGE -a end_page_no=$END_PAGE -a list_name=$LIST_NAME
