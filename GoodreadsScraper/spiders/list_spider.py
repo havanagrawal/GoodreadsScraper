a98cb1d24ba6e8d2c56c9aa874d51c7d5d4491b1
Havan <havanagrawal@gmail.com>
2017-11-11 11:45:38 -0800

    Make list name and page numbers cmd args to ListSpider

--- GoodreadsScraper/spiders/list_spider.py
+++ GoodreadsScraper/spiders/list_spider.py
@@ -7,10 +7,13 @@ GOODREADS_URL_PREFIX = "https://www.goodreads.com"
 class ListSpider(scrapy.Spider):
     name = "list"
 
-    def __init__(self, page_no):
+    def __init__(self, list_name, start_page_no, end_page_no):
         self.book_spider = BookSpider()
-        list_name = "264.Books_That_Everyone_Should_Read_At_Least_Once"
-        self.start_urls = ["https://www.goodreads.com/list/show/{}?page={}".format(list_name, page_no)]
+
+        self.start_urls = []
+        for page_no in range(int(start_page_no), int(end_page_no) + 1):
+            list_url = "https://www.goodreads.com/list/show/{}?page={}".format(list_name, page_no)
+            self.start_urls.append(list_url)
 
     def parse(self, response):
         list_of_books = response.css("a.bookTitle::attr(href)").extract()
