62ed32651ca685966f4861c21db4ac49205fbae4
Havan <havanagrawal+github@gmail.com>
2023-02-05 17:13:19 -0800

    Add new extractors for Book items (partial)
     - This partially addresses the issue in #17

--- GoodreadsScraper/items.py
+++ GoodreadsScraper/items.py
@@ -5,7 +5,9 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 import re
+import json
 import datetime
+from typing import Any, Dict
 
 import scrapy
 from scrapy import Field
@@ -17,10 +19,99 @@ from dateutil.parser import parse as dateutil_parse
 from w3lib.html import remove_tags
 
 
-def num_page_extractor(num_pages):
-    if num_pages:
-        return num_pages.split()[0]
-    return None
+def print_schema(d, tabs=1):
+    if type(d) != dict:
+        return
+    for key in d.keys():
+        print('\t'*tabs + key)
+        print_schema(d[key], tabs+1)
+
+
+def visit_path(data: Dict[str, Any], key: str, original_key: str):
+    """If the generator returned by this function yields None, then no more data is left"""
+
+    # print(f"Processing {key} for {data.keys() if type(data) == dict else data}")
+
+    if not data:
+        # print for debugging
+        if key:
+            print(f'No data found for key {original_key} in data')
+            print(data)
+        return None
+
+    # if no key is left, then yield the data at this point
+    if not key:
+        yield data
+        # stop the generator since there is no more key left to parse
+        return None
+
+    if '.' in key:
+        idx = key.index('.')
+        subkey, remaining_key = key[:idx], key[idx + 1:]
+    else:
+        subkey, remaining_key = key, None
+
+    # handle partial matches on the key
+    # this is needed when the key can be dynamic
+    if subkey.endswith('*'):
+        # remove '*'
+        subkey_prefix = subkey[:-1]
+
+        # find all keys which match subkey_prefix
+        matching_subkeys = [k for k in data.keys() if k.startswith(subkey_prefix)]
+
+        # print for debugging
+        if len(matching_subkeys) > 1:
+            print("Found more than one key matching pattern '{key}'.")
+
+        for sk in matching_subkeys:
+            yield from visit_path(data[sk], remaining_key, original_key)
+
+    elif subkey.endswith('[]'):
+        # TODO(havan): Handle arrays
+
+        # remove '[]'
+        subkey = subkey[:-2]
+
+        values = data.get(subkey, [])
+
+        for value in values:
+            yield from visit_path(value, remaining_key, original_key)
+    # handle regular keys
+    else:
+        yield from visit_path(data.get(subkey, None), remaining_key, original_key)
+
+
+
+def json_field_extractor_v2(key: str):
+    def extract_field(text: str):
+        data = json.loads(text)
+        return list(visit_path(data, key, key))
+    return extract_field
+
+
+def json_field_extractor(key: str):
+    def extract_field(text: str):
+        data = json.loads(text)
+        value = data
+        for subkey in key.split("."):
+            # handle partial matches on the key
+            # this is needed when the key is dynamic
+            if subkey.endswith('*'):
+                subkey_prefix = subkey[:-1]
+                sk = [k for k in value.keys() if k.startswith(subkey_prefix)]
+                if len(sk) > 1:
+                    print("Found more than one key matching pattern '{key}'. Arbitrarily returning the first matched value.")
+                subkey = sk[0]
+            value = value.get(subkey, None)
+            if not value:
+                return None
+        return value
+    return extract_field
+
+
+def splitter(split_on=','):
+    return lambda s: s.split(split_on)
 
 
 def safe_parse_date(date):
@@ -98,32 +189,40 @@ class BookItem(scrapy.Item):
     # Scalars
     url = Field()
 
-    title = Field(input_processor=MapCompose(str.strip))
+    # title = Field(input_processor=MapCompose(str.strip))
+    title = Field(input_processor=MapCompose(json_field_extractor_v2('props.pageProps.apolloState.Book*.title')))
+    title_complete = Field(input_processor=MapCompose(json_field_extractor_v2('props.pageProps.apolloState.Book*.titleComplete')))
+    description = Field(input_processor=MapCompose(json_field_extractor_v2('props.pageProps.apolloState.Book*.description')))
+    image_url = Field(input_processor=MapCompose(json_field_extractor_v2('props.pageProps.apolloState.Book*.imageUrl')))
+    genres = Field(input_processor=MapCompose(json_field_extractor_v2('props.pageProps.apolloState.Book*.bookGenres[].genre.name')), output_processor=Compose(set, list))
+    asin = Field(input_processor=MapCompose(json_field_extractor_v2('props.pageProps.apolloState.Book*.details.asin')))
+    isbn = Field(input_processor=MapCompose(json_field_extractor_v2('props.pageProps.apolloState.Book*.details.isbn')))
+    isbn13 = Field(input_processor=MapCompose(json_field_extractor_v2('props.pageProps.apolloState.Book*.details.isbn13')))
+    publisher = Field(input_processor=MapCompose(json_field_extractor_v2('props.pageProps.apolloState.Book*.details.publisher')))
+    series = Field(input_processor=MapCompose(json_field_extractor_v2('props.pageProps.apolloState.Book*.details.bookSeries')))
+
     author = Field(input_processor=MapCompose(str.strip))
 
-    num_ratings = Field(input_processor=MapCompose(str.strip, int))
-    num_reviews = Field(input_processor=MapCompose(str.strip, int))
-    avg_rating = Field(input_processor=MapCompose(str.strip, float))
-    num_pages = Field(
-        input_processor=MapCompose(str.strip, num_page_extractor, int))
-
-    language = Field(input_processor=MapCompose(str.strip))
+    num_ratings = Field(input_processor=MapCompose(json_field_extractor_v2('aggregateRating.ratingCount')))
+    num_reviews = Field(input_processor=MapCompose(json_field_extractor_v2('aggregateRating.reviewCount')))
+    avg_rating = Field(input_processor=MapCompose(json_field_extractor_v2('aggregateRating.ratingValue')))
+    num_pages = Field(input_processor=MapCompose(json_field_extractor_v2('numberOfPages')))
+    language = Field(input_processor=MapCompose(json_field_extractor_v2('inLanguage')))
+    book_format = Field(input_processor=MapCompose(json_field_extractor_v2('bookFormat')))
     publish_date = Field(input_processor=extract_publish_dates)
 
     original_publish_year = Field(
         input_processor=MapCompose(extract_year, int))
 
-    isbn = Field(input_processor=MapCompose(str.strip, isbn_filter))
-    isbn13 = Field(input_processor=MapCompose(str.strip, isbn13_filter))
-    asin = Field(input_processor=MapCompose(filter_asin))
+    # isbn = Field(input_processor=MapCompose(str.strip, json_field_extractor('isbn'), isbn_filter))
+    # isbn13 = Field(input_processor=MapCompose(str.strip, json_field_extractor('isbn'), isbn13_filter))
 
-    series = Field()
+    # series = Field()
 
     # Lists
-    awards = Field(output_processor=Identity())
+    awards = Field(input_processor=MapCompose(json_field_extractor('awards'), splitter(',')), output_processor=Identity())
     places = Field(output_processor=Identity())
     characters = Field(output_processor=Identity())
-    genres = Field(output_processor=Compose(set, list))
 
     # Dicts
     rating_histogram = Field(input_processor=MapCompose(extract_ratings))
--- GoodreadsScraper/spiders/book_spider.py
+++ GoodreadsScraper/spiders/book_spider.py
@@ -21,40 +21,55 @@ class BookSpider(scrapy.Spider):
         super().__init__()
         self.author_spider = AuthorSpider()
 
-    def parse(self, response):
-        loader = BookLoader(BookItem(), response=response)
+    def parse(self, response, loader=None):
+        if not loader:
+            loader = BookLoader(BookItem(), response=response)
 
         loader.add_value('url', response.request.url)
 
-        loader.add_css("title", "#bookTitle::text")
-        loader.add_css("author", "a.authorName>span::text")
+        # loader.add_css("title", "[data-testid=bookTitle]::text")
+        loader.add_css("author", "span.ContributorLink__name[data-testid=name]::text")
 
-        loader.add_css("num_ratings", "[itemprop=ratingCount]::attr(content)")
-        loader.add_css("num_reviews", "[itemprop=reviewCount]::attr(content)")
-        loader.add_css("avg_rating", "span[itemprop=ratingValue]::text")
-        loader.add_css("num_pages", "span[itemprop=numberOfPages]::text")
+        # loader.add_css("num_ratings", "[data-testid=ratingsCount]::text")
+        # loader.add_css("num_reviews", "[data-testid=reviewsCount]::text")
+        # loader.add_css("avg_rating", "div.RatingStatistics__rating::text")
+        # loader.add_css("num_pages", "[data-testid=pagesFormat]::text")
 
-        loader.add_css("language", "div[itemprop=inLanguage]::text")
-        loader.add_css('publish_date', 'div.row::text')
+        loader.add_css('publish_date', '[data-testid=publicationInfo]::text')
         loader.add_css('publish_date', 'nobr.greyText::text')
 
         loader.add_css('original_publish_year', 'nobr.greyText::text')
 
-        loader.add_css("genres", 'div.left>a.bookPageGenreLink[href*="/genres/"]::text')
-        loader.add_css("awards", "a.award::text")
+        # loader.add_css("genres", 'div.left>a.bookPageGenreLink[href*="/genres/"]::text')
+
         loader.add_css('characters', 'a[href*="/characters/"]::text')
         loader.add_css('places', 'div.infoBoxRowItem>a[href*=places]::text')
-        loader.add_css('series', 'div.infoBoxRowItem>a[href*="/series/"]::text')
-
-        loader.add_css('asin', 'div.infoBoxRowItem[itemprop=isbn]::text')
-        loader.add_css('isbn', 'div.infoBoxRowItem[itemprop=isbn]::text')
-        loader.add_css('isbn', 'span[itemprop=isbn]::text')
-        loader.add_css('isbn', 'div.infoBoxRowItem::text')
-        loader.add_css('isbn13', 'div.infoBoxRowItem[itemprop=isbn]::text')
-        loader.add_css('isbn13', 'span[itemprop=isbn]::text')
-        loader.add_css('isbn13', 'div.infoBoxRowItem::text')
 
         loader.add_css('rating_histogram', 'script[type*="protovis"]::text')
+
+        # The new Goodreads page sends JSON in a script tag
+        # that has these values
+
+        loader.add_css('title', 'script#__NEXT_DATA__::text')
+        loader.add_css('title_complete', 'script#__NEXT_DATA__::text')
+        loader.add_css('description', 'script#__NEXT_DATA__::text')
+        loader.add_css('image_url', 'script#__NEXT_DATA__::text')
+        loader.add_css('genres', 'script#__NEXT_DATA__::text')
+        loader.add_css('asin', 'script#__NEXT_DATA__::text')
+        loader.add_css('isbn', 'script#__NEXT_DATA__::text')
+        loader.add_css('isbn13', 'script#__NEXT_DATA__::text')
+        loader.add_css('publisher', 'script#__NEXT_DATA__::text')
+        loader.add_css('series', 'script#__NEXT_DATA__::text')
+
+        loader.add_css('language', 'script[type*="json"]::text')
+        # loader.add_css('isbn', 'script[type*="json"]::text')
+        # loader.add_css('isbn13', 'script[type*="json"]::text')
+        loader.add_css('num_pages', 'script[type*="json"]::text')
+        loader.add_css("num_ratings", 'script[type*="json"]::text')
+        loader.add_css("num_reviews", 'script[type*="json"]::text')
+        loader.add_css("avg_rating", 'script[type*="json"]::text')
+        loader.add_css("awards", 'script[type*="json"]::text')
+        loader.add_css("book_format", 'script[type*="json"]::text')
 
         yield loader.load_item()
 
